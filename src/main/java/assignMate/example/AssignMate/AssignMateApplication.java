eb9f6a019c3b7e1e9851b0e3416b40193bddc2fa
ayush <gdadvertising2020@gmail.com>
2024-11-03 12:04:24 +0530

    intial commit

---
+++ src/main/java/assignMate/example/AssignMate/AssignMateApplication.java
@@ -0,0 +1,13 @@
+package assignMate.example.AssignMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/assignMate/example/AssignMate/AssignMateApplicationTests.java
@@ -0,0 +1,13 @@
+package assignMate.example.AssignMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
